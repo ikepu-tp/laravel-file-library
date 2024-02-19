10791e1b67b29054ee329eaf311ab9a57dc80bca
ikepu-tp <postmaster@licsss.jp>
2024-02-20 06:57:16 +0900

    add route except

--- src/routes/route.php
+++ src/routes/route.php
@@ -7,9 +7,9 @@ $middleware = config("file-library.middleware");
 if (is_string($middleware)) $middleware = [$middleware];
 Route::group([
     "middleware" => array_merge(
-        ["guard:" . config("file-library.guard")],
+        ["auth:" . config("file-library.guard")],
         $middleware,
     ),
 ], function () {
-    Route::resource("file/lib", FileController::class)->names("file-library");
+    Route::resource("file/lib", FileController::class)->names("file-library")->except(config("file-library.route_except", []));
 });
