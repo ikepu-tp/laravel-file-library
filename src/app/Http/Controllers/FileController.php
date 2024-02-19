8f896ea65f5255a1789c3daf58c4bf6c51a4438b
ikepu-tp <postmaster@licsss.jp>
2024-02-20 06:46:56 +0900

    update file upload

--- src/app/Http/Controllers/FileController.php
+++ src/app/Http/Controllers/FileController.php
@@ -3,6 +3,7 @@
 namespace ikepu_tp\FileLibrary\app\Http\Controllers;
 
 use App\Http\Controllers\Controller as BaseController;
+use Exception;
 use ikepu_tp\FileLibrary\app\Http\Requests\FileRequest;
 use ikepu_tp\FileLibrary\app\Models\File;
 use Illuminate\Foundation\Auth\User;
@@ -32,6 +33,8 @@ class FileController extends BaseController
     public function store(FileRequest $fileRequest)
     {
         $guard = config("file-library.guard");
+        /** @var bool */
+        $upload_failed = false;
         /** @var \Illuminate\Foundation\Auth\User */
         $user = $fileRequest->user($guard);
         $user_id = $user->getKey();
@@ -39,20 +42,32 @@ class FileController extends BaseController
         $files = $fileRequest->file("files", []);
         /** @var string[] */
         $names = $fileRequest->input("names", []);
-        foreach ($files as $idx => $file) {
+        /** @var File[] */
+        $saved_files = [];
+        foreach ($files as $idx => $upfile) {
             $file_model = new File();
             $fileId = Str::uuid();
-            $path = config("file-library.path", "") . $fileId;
+            $path = config("file-library.path", "");
+            $name = $fileId . "." . $upfile->getClientOriginalExtension();;
             $file_model->fill([
                 "fileId" => $fileId,
                 "user_id" => $user_id,
                 "guard" => $guard,
                 "name" => isset($names[$idx]) ? $names[$idx] : "",
-                "type" => "",
-                "path" => $path,
+                "type" => $upfile->getClientMimeType(),
+                "path" => "{$path}/{$name}",
             ]);
-            Storage::putFileAs($path, $file);
+            if (!$upfile->storeAs($path, $name) || !$file_model->save()) {
+                $upload_failed = true;
+            } else {
+                $saved_files[] = $file_model;
+            }
         }
+
+        if ($upload_failed) throw new Exception("Upload failed.");
+
+        if ($fileRequest->expectsJson()) return $saved_files;
+        return back()->with("status", "File saved.");
     }
 
     /**
