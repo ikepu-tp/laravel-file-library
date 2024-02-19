215c8a2e636c726933de8c1d0fa735c42b650fdd
ikepu-tp <postmaster@licsss.jp>
2024-02-20 06:52:45 +0900

    add route

--- src/routes/route.php
+++ src/routes/route.php
@@ -1,6 +1,15 @@
 <?php
 
+use ikepu_tp\FileLibrary\app\Http\Controllers\FileController;
 use Illuminate\Support\Facades\Route;
 
-Route::group([], function () {
+$middleware = config("file-library.middleware");
+if (is_string($middleware)) $middleware = [$middleware];
+Route::group([
+    "middleware" => array_merge(
+        ["guard:" . config("file-library.guard")],
+        $middleware,
+    ),
+], function () {
+    Route::resource("file/lib", FileController::class)->names("file-library");
 });
