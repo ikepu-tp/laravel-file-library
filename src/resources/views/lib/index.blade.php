31e4216fc69fdee5d6f2186cc06ab05d997359bc
ikepu-tp <postmaster@licsss.jp>
2024-03-02 13:53:01 +0900

    add donwload link

--- src/resources/views/lib/index.blade.php
+++ src/resources/views/lib/index.blade.php
@@ -12,19 +12,24 @@
     </div>
     <div class="row">
       @foreach ($files as $file)
-        <div class="col-auto">
+        <div class="col-auto col-sm-12 col-md-6 col-lg-6 col-xl-4">
           <div class="card my-2">
             <div class="card-header">
               <a href="{{ route('file-library.show', ['file' => $file->fileId]) }}" target="_blank">
                 {{ $file->name }}
               </a>
             </div>
-            <a href="{{ route('file-library.show', ['file' => $file->fileId]) }}" class="card-body" target="_blank">
+            <div class="card-body">
               <iframe src="{{ route('file-library.show', ['file' => $file->fileId]) }}" frameborder="0"
-                class="border-0"></iframe>
-            </a>
+                class="border-0 w-100"></iframe>
+            </div>
             <div class="card-footer">
-              <div class="row justify-content-end">
+              <div class="row justify-content-between">
+                <div class="col-auto">
+                  <a href="{{ route('file-library.download', ['file' => $file]) }}" target="_blank"
+                    rel="noopener noreferrer">
+                    ダウンロード</a>
+                </div>
                 <div class="col-auto text-secondary" style="font-size: .75em;">
                   <div class="text-end">
                     {{ __('FileLibrary::file-library.created_at') }}：{{ $file->created_at->format('Y/m/d H:i') }}
